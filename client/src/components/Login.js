c5d2afc490f6f51c1389acbe45b4cafd02729552
Matt <mattlaws1982@gmail.com>
2020-08-03 15:32:54 +0100

    frontend stuff in place

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,14 +5,27 @@ const Login = (props) => {
     
     return(
       <div className="login">
-        <h2>Login</h2>
+        <div className="login-inner">
+          <div className="login-left">
+            <h2>Quizzical</h2>
+            <h4>Ready to test your brain against<br /> our skull busting question?</h4>
+          </div>
+          <div className="login-right">
+            <div className="login-heading">
+              <i class="fas fa-user-lock fa-2x"></i>
+              <h2>Login</h2>
+            </div>
             <form>
-            <input type="email" name="userEmail" placeholder="Enter email..." onChange={props.setData}/>
-            <input type="password" name="userPassword" placeholder="Enter password..." onChange={props.setData}/>
-            <button type="submit" onClick={props.loginForm}>LOG IN</button>
+                <input type="email" name="userEmail" placeholder="Enter email..." onChange={props.setData} />
+
+                <input type="password" name="userPassword" placeholder="Enter password..." onChange={props.setData}/>
+                <button type="submit" onClick={props.loginForm}>LOG IN</button>
             </form>
             <p>Not registered? Head on over to our register page.</p>
-      </div>
+          </div>
+          
+        </div>
+        </div>
     )
 }
 
